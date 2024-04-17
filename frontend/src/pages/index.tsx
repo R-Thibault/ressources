844c9809a0bf9a6c32eb14c112e4b43486154f68
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2024-04-17 10:11:45 +0200

    update-docker-compose-prod- Test automatique deployement on staging

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/organisms/layout";
 export default function Home(): React.ReactNode {
   return (
     <Layout title={"Accueil"}>
-      <div>Ceci est une div</div>
+      <div>Ceci est une page web</div>
     </Layout>
   );
 }
