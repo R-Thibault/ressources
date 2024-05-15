b0860099949e3a8192f8d06d4042304c14592e76
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2024-05-15 14:33:06 +0200

    19-user-dashboard- 19-user-dashboard- Create ressource - wip

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -44,7 +44,7 @@ export class User extends BaseEntity {
   @OneToOne(() => Image)
   @JoinColumn()
   @Field(() => Image)
-  image_id!: Image;
+  avatar!: Image;
 
   @Column({ type: "varchar", length: 255, nullable: true })
   @Field(() => String)
